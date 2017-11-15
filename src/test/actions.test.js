839c5f4f2c402299c8970d4b4ad2b4c38c91e5b0
Steve Kinney <hello@stevekinney.net>
2017-11-15 11:36:42 -0700

    Add action testing

---
+++ src/test/actions.test.js
@@ -0,0 +1,126 @@
+import _ from 'lodash';
+
+import * as constants from '../constants';
+
+import {
+  addNewItem,
+  toggleItem,
+  removeItem,
+  markAllAsUnpacked
+} from '../actions/items-actions';
+
+import {
+  updateUnpackedItemsFilter,
+  updatePackedItemsFilter,
+} from '../actions/filter-actions';
+
+import {
+  updateNewItemValue,
+} from '../actions/new-item-actions'
+
+describe('Item Actions', () => {
+
+  describe('addNewItem', () => {
+    it('should return ADD_NEW_ITEM as the item type', () => {
+      const action = addNewItem('item name');
+      expect(action.type).toBe(constants.ADD_NEW_ITEM);
+    });
+
+    it('should return the provided value on the action', () => {
+      const value = 'item name';
+      const action = addNewItem(value);
+      expect(action.value).toBe(value);
+    });
+
+    it('should default packed on the result action', () => {
+      const action = addNewItem('item name');
+      expect(action.packed).toBe(false);
+    });
+
+    it('provide a string id', () => {
+      const action = addNewItem('item name');
+      expect(typeof action.id).toBe('string');
+    });
+  });
+
+  describe('toggleItem', () => {
+    it('should return TOGGLE_ITEM as the item type', () => {
+      const action = toggleItem('item name');
+      expect(action.type).toBe(constants.TOGGLE_ITEM);
+    });
+
+    it('should return the provided id on the action', () => {
+      const id = '123';
+      const action = toggleItem(id);
+      expect(action.id).toBe(id);
+    });
+  });
+
+  describe('removeItem', () => {
+    it('should return REMOVE_ITEM as the item type', () => {
+      const action = removeItem('item name');
+      expect(action.type).toBe(constants.REMOVE_ITEM);
+    });
+
+    it('should return the provided id on the action', () => {
+      const id = '123';
+      const action = removeItem(id);
+      expect(action.id).toBe(id);
+    });
+  });
+
+  describe('markAllAsUnpacked', () => {
+    it('should return MARK_ALL_AS_UNPACKED as the item type', () => {
+      const action = markAllAsUnpacked('item name');
+      expect(action.type).toBe(constants.MARK_ALL_AS_UNPACKED);
+    });
+  });
+
+});
+
+describe('New Item Actions', () => {
+
+  describe('updateNewItemValue', () => {
+    it('should return UPDATE_NEW_ITEM_VALUE as the item type', () => {
+      const action = updateNewItemValue('item name');
+      expect(action.type).toBe(constants.UPDATE_NEW_ITEM_VALUE);
+    });
+
+    it('should return the provided text', () => {
+      const value = 'item name';
+      const action = updateNewItemValue(value);
+      expect(action.value).toBe(value);
+    });
+  });
+
+});
+
+describe('Filter Actions', () => {
+
+  describe('updateUnpackedItemsFilter', () => {
+    it('should return UPDATE_UNPACKED_ITEMS_FILTER as the item type', () => {
+      const action = updateUnpackedItemsFilter('item name');
+      expect(action.type).toBe(constants.UPDATE_UNPACKED_ITEMS_FILTER);
+    });
+
+    it('should return the provided text', () => {
+      const text = 'item name';
+      const action = updateUnpackedItemsFilter(text);
+      expect(action.text).toBe(text);
+    });
+  });
+
+  describe('updatePackedItemsFilter', () => {
+    it('should return UPDATE_PACKED_ITEMS_FILTER as the item type', () => {
+      const action = updatePackedItemsFilter('item name');
+      expect(action.type).toBe(constants.UPDATE_PACKED_ITEMS_FILTER);
+    });
+
+    it('should return the provided text', () => {
+      const text = 'item name';
+      const action = updatePackedItemsFilter(text);
+      expect(action.text).toBe(text);
+    });
+  });
+
+});
