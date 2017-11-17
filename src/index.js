616a8befbb8da487c6427d47ea01590ecc6019b6
Steve Kinney <hello@stevekinney.net>
2017-11-17 05:42:53 -0600

    Wire up store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
 import Application from './components/Application';
+import store from './store';
 
 import './index.css';
 
-ReactDOM.render(<Application />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <Application />
+  </Provider>
+, document.getElementById('root'));
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,16 @@
-import { createStore } from 'redux';
+import { applyMiddleware, compose, createStore } from 'redux';
 
 import reducers from '../reducers';
 import initialState from './initial-state';
 
-export default createStore(
+const middleware = [];
+const enhancers = [];
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
   reducers,
-  initialState
+  initialState,
+  composeEnhancers(applyMiddleware(...middleware), ...enhancers),
 );
+
+export default store;
